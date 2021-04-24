0617f83d5c25bf2e23879e2b25bc0b4a9369c8a2
AliOcasha <aliocasha@gmail.com>
2021-04-24 20:31:00 +0200

    Implementing the Ray Spreading

--- Raycasting/Assets/RayScript.cs
+++ Raycasting/Assets/RayScript.cs
@@ -7,7 +7,8 @@ public class RayScript : MonoBehaviour
     private LineRenderer Ray;
     private Vector3 MousePos;
     private Vector3 WorldPos;
-    private Vector3 offset = new Vector3(0.1f, 0.1f, 0);
+    private Vector3 Offset = new Vector3(0.1f, 0.1f, 0);
+    private Vector3 C_Offset = Vector3.zero;
     // Start is called before the first frame update
     private void Start()
     {
@@ -15,12 +16,20 @@ public class RayScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
+        if (C_Offset.x < Offset.x)
+        {
+            C_Offset.x += 0.005f;
+        }
+        if (C_Offset.y < Offset.y)
+        {
+            C_Offset.y += 0.005f;
+        }
         MousePos = Input.mousePosition;
         MousePos.z = Camera.main.nearClipPlane;
         WorldPos = Camera.main.ScreenToWorldPoint(MousePos);
         Ray.SetPosition(0, WorldPos);
-        Ray.SetPosition(1, WorldPos + offset);
+        Ray.SetPosition(1, WorldPos + C_Offset);
     }
 }
