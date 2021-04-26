294658f7ec2440f11e8bc0876a7184035b3d2069
AliOcasha <aliocasha@gmail.com>
2021-04-26 18:15:55 +0200

    Structured and commited Code

--- Raycasting/Assets/Scripts/Physics.cs
+++ Raycasting/Assets/Scripts/Physics.cs
@@ -15,7 +15,7 @@ public class Physics : MonoBehaviour
     private Vector3 Direction = Vector3.zero;
     private Vector3 B_Direction = Vector3.zero;
 
-
+    // Creating n Rays at Start and Detecting B_Direction
     private void Start()
     {
         for (int i = 0; i < maxRayCount; i++)
@@ -26,23 +26,32 @@ public class Physics : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Getting all Rays in Scene
         AllRays = GameObject.FindGameObjectsWithTag("Ray");
         for (int i = 0; i < AllRays.Length; i++)
         {
+            // Distrubuting Rays evenly around Mouse Position
             Direction.x = Mathf.Sin(i * 2 * Mathf.PI / maxRayCount);
             Direction.y = Mathf.Cos(i * 2 * Mathf.PI / maxRayCount);
+            // Getting Line Renderer of current Ray
             RayDraw = AllRays[i].GetComponent<LineRenderer>();
-            intersection(RayDraw, Boundary);
+            // Detecting intersection with Boundary
+            Intersection(RayDraw, Boundary);
+            // Drawing Ray
             Rays.DrawRay(RayDraw, Direction*Length);
             
         }
 
     }
 
-    void intersection(LineRenderer R, LineRenderer B)
+    void Intersection(LineRenderer R, LineRenderer B)
     {
+        // Trying to Detect a Collision with a Boundary and modifying Length as Reaction
+        // NOT WORKING -- Currently makin invisble Barrier in middle of Screen
         u = B.GetPosition(0).x;
         cLength = (Boundary.GetPosition(0).x + B_Direction.x * u - R.GetPosition(0).x) / Direction.x;
+        // Only Apply Resicing when Ray is poiting in right direction
+        // Limpiting Ray Length to 5 cause of Unity not liking infinite Rays
         if (cLength > 0)
         {
             if (cLength < 5)
--- Raycasting/Assets/Scripts/Ray.cs
+++ Raycasting/Assets/Scripts/Ray.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ray : MonoBehaviour
 {    
     private Vector3 MousePos;
     private Vector3 WorldPos;
+
+    // Creating a Ray
     public void CreateRay()
     {
         Instantiate(gameObject);
@@ -13,10 +13,12 @@ public class Ray : MonoBehaviour
 
     public void DrawRay(LineRenderer Ray, Vector3 Direction)
     {
+        // Projection Mouse Position from Raw Window Position into Camera and World
         MousePos = Input.mousePosition;
         MousePos.z = Camera.main.nearClipPlane;
         WorldPos = Camera.main.ScreenToWorldPoint(MousePos);
         Ray.SetPosition(0, WorldPos);
+        // Direction and Length of Ray depending on Input
         Ray.SetPosition(1, Ray.GetPosition(0) + Direction);
     }
 }
