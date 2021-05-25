f11f6e0994184e12e2069cdb459573dd5ab5afca
AliOcasha <aliocasha@gmail.com>
2021-05-25 12:35:54 +0200

    Redone Raycasting from fixed Point and implemented funcitonality with multiple Boundaries

--- Raycasting-Python/Raycasting/Raycasting/Raycasting.py
+++ Raycasting-Python/Raycasting/Raycasting/Raycasting.py
@@ -9,41 +9,42 @@ WHITE = (255, 255, 255)
 class Ray:
    def __init__(self):
        self.pos = (WIN_WIDTH/2, WIN_HEIGHT/2)
-       self.rawpos = (0,0)
+       self.rawpos = pg.mouse.get_pos()
        self.x = 0.1
        self.y = 0.1
        self.xr = 0.1
        self.yr = 0.1
-       self.factor = 10
-       
+       self.factor = 100
+       self.fl = [100]
+
 
    def move(self):
        self.rawpos = pg.mouse.get_pos()
-       self.xr = self.rawpos[0]
-       self.yr = self.rawpos[1]
+       self.xr = pg.mouse.get_pos()[0] - WIN_WIDTH/2
+       self.yr = pg.mouse.get_pos()[1] - WIN_HEIGHT/2
        self.rawpos = (self.xr, self.yr)
 
 
    def draw(self, screen):
        screen.fill((0,0,0))        
-       self.x = self.rawpos[0] + 100* self.factor
-       self.y = self.rawpos[1] + 0 * self.factor
+       self.x = self.rawpos[0] * self.factor + WIN_WIDTH/2
+       self.y = self.rawpos[1] * self.factor + WIN_HEIGHT/2
        self.pos = (self.x, self.y)      
-       pg.draw.line(screen, WHITE, pg.mouse.get_pos(), self.pos)
+       pg.draw.line(screen, WHITE, (WIN_WIDTH/2, WIN_HEIGHT/2), self.pos)
 
-   def checkIntersection(self, Wall):
-       WallIntersectionParameter = (self.rawpos[0]*(Wall.p1[1]-pg.mouse.get_pos()[1]) + self.rawpos[1]*(pg.mouse.get_pos()[0]-Wall.p1[0]))/((Wall.p2[0]-Wall.p1[0])*self.rawpos[1] - (Wall.p2[1]-Wall.p1[1])*self.rawpos[0])
+   def checkIntersection(self, Walls):
+       self.fl = [100]
+       for Wall in Walls:
+           WallIntersectionParameter = (self.rawpos[0]*(Wall.p1[1]-WIN_HEIGHT/2) + self.rawpos[1]*(WIN_WIDTH/2-Wall.p1[0]))/((Wall.p2[0]-Wall.p1[0])*self.rawpos[1] - (Wall.p2[1]-Wall.p1[1])*self.rawpos[0])
+           if WallIntersectionParameter > 0 and WallIntersectionParameter < 1:
+               RayIntersection = (Wall.p1[0]+(Wall.p2[0]-Wall.p1[0])*WallIntersectionParameter-WIN_WIDTH/2)/self.rawpos[0]
+               if RayIntersection > 0:
+                   if RayIntersection in self.fl:
+                       pass
+                   else:
+                       self.fl.append(RayIntersection)
 
-       if WallIntersectionParameter > 0 and WallIntersectionParameter < 1:
-           RayIntersection = (Wall.p1[0]+(Wall.p2[0]-Wall.p1[0])*WallIntersectionParameter-pg.mouse.get_pos()[0])/self.rawpos[0]
-           if RayIntersection > 0:
-               self.factor = RayIntersection
-           else:
-               self.factor = 10
-               self.x = self.rawpos[0] + 100* self.factor
-               self.y = self.rawpos[1] + 50* self.factor
-               self.pos = (self.x, self.y)
-
+       self.factor = min(self.fl)
 
 class Boundary:
     def __init__(self, p1, p2):
@@ -67,7 +68,9 @@ def main():
     clock = pg.time.Clock()
 
     ray = Ray()
-    Walls = [Boundary((200,150), (250,500))]
+    Walls = [Boundary((200,150), (250,500)),
+             Boundary((400,200), (50,300)),
+             Boundary((600,550), (800,450))]
 
     while Run:
         clock.tick(30)
@@ -77,7 +80,7 @@ def main():
                 pg.quit()
                 sys.exit(0)
         ray.move()
-        ray.checkIntersection(Walls[0])
+        ray.checkIntersection(Walls)
         Draw_Window(screen, ray, Walls)
 
 main()
