1573ad9314206321e458690653f8e85ca0c0070f
AliOcasha <aliocasha@gmail.com>
2021-06-21 20:57:58 +0200

    Added Error Catching for ZeroDivision and approximation by division with 0.001

--- Raycasting.py
+++ Raycasting.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import sys
 pg.init()
 
 WIN_WIDTH = 900
@@ -41,8 +42,11 @@ class Ray:
        for Wall in Walls:
            WallZaehler = self.rawpos[0]*(Wall.p1[1]-WIN_HEIGHT/2) + self.rawpos[1]*(WIN_WIDTH/2-Wall.p1[0])
            WallNenner = (Wall.p2[0]-Wall.p1[0])*self.rawpos[1] - (Wall.p2[1]-Wall.p1[1])*self.rawpos[0]
+           try:
+                WallIntersectionParameter = WallZaehler/WallNenner
+           except ZeroDivisionError:
+                WallIntersectionParameter = WallZaehler/0.001
 
-           WallIntersectionParameter = WallZaehler/WallNenner
 
            if WallIntersectionParameter > 0 and WallIntersectionParameter < 1:
                RayIntersection = (Wall.p1[0]+(Wall.p2[0]-Wall.p1[0])*WallIntersectionParameter-WIN_WIDTH/2)/self.rawpos[0]
